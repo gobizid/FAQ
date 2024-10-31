6adac68325ec615b151212d30705a291ccbb84e0
qintharganteng <qintharalmaliki@gmail.com>
2024-10-31 10:52:08 +0700

    Add OpenLayers integration: Initialize map and update structure

---
+++ assets/js/openlayer.js
@@ -0,0 +1,20 @@
+import Map from 'https://cdn.skypack.dev/ol/Map.js';
+import View from 'https://cdn.skypack.dev/ol/View.js';
+import TileLayer from 'https://cdn.skypack.dev/ol/layer/Tile.js';
+import OSM from 'https://cdn.skypack.dev/ol/source/OSM.js';
+import { fromLonLat, toLonLat } from 'https://cdn.skypack.dev/ol/proj.js';
+import Overlay from 'https://cdn.skypack.dev/ol/Overlay.js';
+
+// Initialize the map
+const map = new Map({
+    target: 'map',
+    layers: [
+        new TileLayer({
+            source: new OSM()
+        })
+    ],
+    view: new View({
+        center: fromLonLat([107.6098, -6.9175]),
+        zoom: 12
+   })
+});
